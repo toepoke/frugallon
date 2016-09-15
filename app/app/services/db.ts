b43c10094bf35bb157f4d9bcdfc0c7cbeafd819f
franz <franz@junge.f9.co.uk>
2016-09-15 16:38:32 +0100

    chore(merge): Removed old App folder

--- app/app/services/db.ts
+++
@@ -1,6 +0,0 @@
-export { CarsDb } from "./db/car-db";
-export { CarMakersDb } from "./db/car-maker-db";
-export { FillsDb } from "./db/fills-db";
-export { SettingsDb } from "./db/settings-db";
-export { MpgStatsDb } from "./db/mpg-stats-db";
-
