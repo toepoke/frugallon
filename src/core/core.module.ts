9dafda29c9f9898f28945ec4ac2eafb7719da7b1
franz <franz@junge.f9.co.uk>
2017-01-24 18:05:02 +0000

    added Core module for adding compoents from the core namespace

---
+++ src/core/core.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BaseDb, DbProviders, DbCmdSuccess, DbCmdFailure, TypedDb, DbTypes } from './typed-db';
+
+@NgModule({
+	imports: [
+		CommonModule
+	],
+	declarations: [
+	],
+	exports: [
+
+
+	]
+})
+export class CoreModule {
+
+}
