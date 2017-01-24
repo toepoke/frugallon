f4b5c3efeb366562002cdf08b2860f011f5ae509
franz <franz@junge.f9.co.uk>
2017-01-24 11:59:26 +0000

    added store classes

---
+++ src/bricks/stores/actions/app-actions.ts
@@ -0,0 +1,140 @@
+import { Injectable } from '@angular/core';
+import { Action } from '@ngrx/store';
+import { Car, FillUp, eFillUpType, MpgStat, MpgStats } from "../../models";
+import { IAppState } from "../iapp-state";
+
+/**
+ * Set of actions for acting upon the application UI state.
+ */
+@Injectable()
+export class AppActions {
+
+	static CHANGE_MEASUREMENT: string = 'CHANGE_MEASUREMENT';
+	public ChangeMeasurement(measurement: boolean): Action {
+		return {
+			type: AppActions.CHANGE_MEASUREMENT,
+			payload: measurement
+		}
+	}
+
+	static CAR_ADD: string = 'CAR_ADD';
+	public CarAdd(car: Car): Action {
+		return {
+			type: AppActions.CAR_ADD,
+			payload: {
+				car: car
+			}
+		}
+	}
+
+	static CAR_SAVE: string = 'CAR_SAVE';
+	public CarSave(car: Car): Action {
+		return {
+			type: AppActions.CAR_SAVE,
+			payload: {
+				car: {
+					id: car.id,
+					type: car.type, 
+					make: car.make,
+					model: car.model,
+					colour: car.colour,
+					mileage: car.mileage
+				}
+			}
+		}
+	} // CarSave
+
+	static CAR_EDIT: string = 'CAR_EDIT';
+	public CarEdit(car: Car): Action {
+		return {
+			type: AppActions.CAR_EDIT,
+			payload: car
+		}
+
+	} // CarEdit
+
+	static TOGGLE_HISTORY_ITEM: string = 'TOGGLE_HISTORY_ITEM';
+	public ToggleHistoryItem(carId: number): Action {
+		return {
+			type: AppActions.TOGGLE_HISTORY_ITEM,
+			payload: carId
+		}
+	}
+
+	static SHOW_YEAR_VIEW: string = 'SHOW_YEAR_VIEW';
+	public ShowYearView(fills: Array<FillUp>, years?: Array<number>, selectedYear?: number): Action {
+		return {
+			type: AppActions.SHOW_YEAR_VIEW,
+			payload: {
+				filtersActive: false,
+				fills: fills,
+				years: years,
+				selectedYear: selectedYear
+			}
+		}
+
+	}
+
+	static SHOW_FILTERED_VIEW: string = 'SHOW_FILTERED_VIEW';
+	public ShowFilteredView(filteredFills: Array<FillUp>, years: Array<number>): Action {
+		return {
+			type: AppActions.SHOW_YEAR_VIEW,
+			payload: {
+				filtersActive: true,
+				fills: filteredFills,
+				years: years
+			}
+		}
+	}
+
+	static SHOW_MESSAGE: string = 'SHOW_MESSAGE';
+	public ShowMessage(msg: string): Action {
+		return {
+			type: AppActions.SHOW_MESSAGE,
+			payload: msg
+		}
+	}
+
+	static HIDE_MESSAGE: string = 'HIDE_MESSAGE';
+	public HideMessage(): Action {
+		return {
+			type: AppActions.HIDE_MESSAGE
+		}
+	}
+
+
+
+	// Note:
+	// The following aren't subject to reducers.  They _could_ be, they just aren't
+	// These are probably "side effects" in ngrx v2
+	static ADD_FILL_UP: string = 'ADD_FILL_UP';
+	public AddFillUp(fillUp: FillUp): Action {
+		return {
+			type: AppActions.ADD_FILL_UP,
+			payload: fillUp
+		}
+	}
+
+	static ADD_STATS: string = 'ADD_STATS';
+	public AddStats(stats: MpgStats): Action {
+		return {
+			type: AppActions.ADD_STATS,
+			payload: {
+				stats: stats
+			}
+		}
+	}
+
+	static SAVE_STATS: string = 'SAVE_STATS';
+	public SaveStats(stats: MpgStats): Action {
+		return {
+			type: AppActions.SAVE_STATS,
+			payload: {
+				stats: stats
+			}
+		}
+	}
+
+} // AppActions
+
+
---
+++ src/bricks/stores/ifilter-state.ts
@@ -0,0 +1,96 @@
+import { eFillUpType, FillUp, MpgStat, Car } from "../models";
+import * as ditto from "../../core/helpers/ditto";
+
+export interface IFilterState {
+	/** @description - Filters are on or off */
+	filtersActive: boolean;	
+	
+	/** @description - Filter by year (empty => show all) */
+	filteredYears: Array<number>;
+
+	/** @description - Filter by motorway, commute, etc (empty => show all) */
+	filteredJourneyTypes: Array<eFillUpType>;
+
+	/** @description - Filter by average mpg (-1 = bad, 0 = neutral, 1 = good) (empty => show all) */
+	filteredMpgAverages: Array<number>;
+
+	/** @description - Filter by car */
+	filteredCarIds: Array<number>;
+
+}
+
+
+// export function applyFilters(fills: Array<FillUp>, filters: IFilterState, activeMeasurement: boolean): Array<FillUp> {
+// 	let filtered: Array<FillUp> = null;
+	
+// 	filtered = applyYearFilter(fills, filters.filteredYears);
+// 	filtered = applyJourneyFilter(filtered, filters.filteredJourneyTypes);
+// 	filtered = applyMPGFilter(filtered, filters.filteredMpgAverages, activeMeasurement);
+// 	filtered = applyCarFilter(filtered, filters.filteredCarIds);
+	
+// 	return filtered;
+// }
+
+
+// export function applyCarFilter(fills: Array<FillUp>, carIds: Array<number>): Array<FillUp> {
+// 	let filtered: Array<FillUp> = null;
+
+// 	filtered = fills.filter((f: FillUp) => {
+// 		return ditto.any(
+// 			carIds.filter((id: number) => id == f.carId)
+// 		)
+// 	})
+
+// 	return filtered;
+// }
+
+// export function applyYearFilter(fills: Array<FillUp>, years: Array<number>): Array<FillUp> {
+// 	let filtered: Array<FillUp> = null;
+
+// 	filtered = fills.filter((f: FillUp) => {
+// 		return ditto.any(
+// 			years.filter((year: number) => year == f.when.getFullYear())
+// 		)
+// 	});
+
+// 	return filtered;
+// }
+
+
+// export function applyJourneyFilter(fills: Array<FillUp>, journeyTypes: Array<eFillUpType>): Array<FillUp> {
+// 	let filtered: Array<FillUp> = null;
+
+// 	filtered = fills.filter((f: FillUp) => {
+// 		return ditto.any(
+// 			journeyTypes.filter((jt: eFillUpType) => {
+// 				return jt == f.fillType
+// 			})
+// 		);
+// 	});
+
+// 	return filtered;
+// }
+
+
+// export function applyMPGFilter(fills: Array<FillUp>, mpgAverage: Array<number>, measurement: boolean): Array<FillUp> {
+// 	let filtered: Array<FillUp> = null;
+
+// 	filtered = fills.filter((f: FillUp) => {
+// 		let mpg: number = f.getMpg(measurement);
+// 		let stats: MpgStat = f.getMpgStats(measurement);
+// 		let include: boolean = false;
+
+// 		mpgAverage.forEach((avg: number) => {
+// 			if (avg < 0 && stats.isUnderAverage(mpg))
+// 				include = true;
+// 			else if (avg == 0 && stats.isAverage(mpg))
+// 				include = true;
+// 			else if (avg > 0 && stats.isAboveAverage(mpg))
+// 				include = true; 
+// 		});
+		
+// 		return include;
+// 	});
+
+// 	return filtered;
+// }
