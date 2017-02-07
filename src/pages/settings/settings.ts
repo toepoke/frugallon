599a93e6c94bd7faaf13d2b786718f3c8f548e14
franz <franz@junge.f9.co.uk>
2017-02-07 16:57:00 +0000

    Settings page wired up

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,3 +1,11 @@
+import { DbCmdFailure } from './../../core/typed-db/db-responses';
+import { Settings } from './../../bricks/models/settings';
+import { SettingDb } from './../../bricks/db2/setting-db';
+import { AppActions } from './../../bricks/stores/actions/app.actions';
+import { IAppState } from './../../bricks/stores/iapp.state';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
 import { AppService } from './../../bricks/services/app.service';
 import { Component } from '@angular/core';
 
@@ -16,20 +24,54 @@ import { NavController, Alert, AlertController } from 'ionic-angular';
   <ion-content padding>
 
     <ion-list>
-      <button ion-button color="danger" (click)="shouldNukeDatabase()">Nuke Database</button>
+      <ion-item>
+        <ion-label>{{(_app$|async)?.measurementType}} measurement</ion-label>
+        <ion-toggle [checked]="(_app$|async)?.measurement" (ionChange)="onMeasureChange($event)"></ion-toggle>
+      </ion-item>
+      
+      <ion-item>
+        <ion-label>Nuke Database</ion-label>
+        <button item-right ion-button color="danger" (click)="shouldNukeDatabase()">Nuke it!</button>
+      </ion-item>
     </ion-list>
 
   </ion-content>
 `  
 })
 export class SettingsPage {
+	private _app$: Observable<IAppState> = null;
+  private _app$subscription: Subscription = null;
 
   constructor(
     protected _navCtrl: NavController,
     protected _alertCtrl: AlertController,
-    protected _appService: AppService 
+    protected _appService: AppService,
+    protected _settingDb: SettingDb,
+		protected _store: Store<IAppState>,
+    protected _appActions: AppActions
   ) {
+		this._app$ = <Observable<IAppState>> _store.select("appState");
+		this._app$.subscribe((data: IAppState) => {
+		});
+  }
 
+  protected onMeasureChange(evt: any): void {
+    let newMeasurement: boolean = evt.checked;
+
+    this._settingDb.load()
+      .then((current: Settings) => {
+        current.measurement = newMeasurement;
+        return this._settingDb.save(current);
+      })
+      .then(() => {
+        this._store.dispatch(
+          this._appActions.ChangeMeasurement( newMeasurement )
+        );
+      })
+      .catch((fail: DbCmdFailure) => {
+        // TODO:
+      })
+    ;
   }
 
   protected shouldNukeDatabase(): void {
@@ -67,6 +109,7 @@ export class SettingsPage {
         acknowledge.present();
       })
     ;
-  }
+
+  } // nukeDatabase
 
 }
