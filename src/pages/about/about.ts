36282ff973f590b82c1b2ea851d002fd7811d4f0
franz <franz@junge.f9.co.uk>
2017-02-07 16:13:09 +0000

    About page fully working

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,27 +1,52 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
+import { NavController, Platform } from 'ionic-angular';
 
-import { NavController } from 'ionic-angular';
+import { IAppState } from '../../bricks/stores';
+import { DbProviders, SettingDb } from '../../bricks/db2';
+import * as _ from '../../core/helpers/underscore';
 
 @Component({
   selector: 'page-about',
-  template:
-`
-  <ion-header>
-    <ion-navbar>
-      <ion-title>About</ion-title>
-    </ion-navbar>
-  </ion-header>
-  
-  <ion-content padding>
-
-    
-  </ion-content>
-`  
+  templateUrl: 'about.html',
+	styles: [`
+		.divider-header {
+			margin-top: 1rem;
+			float: left;
+			display: inline-block;
+		}
+		.divider-icon {
+			float: right;
+			font-size: 30px;
+			color: #AE5190;
+			display: inline-block;
+		}
+		ul.points li {
+			margin-left: -2rem;
+		}
+	`],   
 })
 export class AboutPage {
+	private _app$: Observable<IAppState> = null;
+  private _app$subscription: Subscription = null;
+	private _dbType: string = '';
+	private _platformNames: Array<string> = null;
+  
+  constructor(
+		private _store: Store<IAppState>, 
+		private _platform: Platform,
+    private _settingDb: SettingDb
+  ) {
+		this._app$ = <Observable<IAppState>> _store.select("appState");
+		this._app$.subscribe((data: IAppState) => {
+			if (_.isPresent(data)) {
 
-  constructor(public navCtrl: NavController) {
-
+			}
+		});
+    this._dbType = DbProviders.getDescription( this._settingDb.getActiveProvider() );
+    this._platformNames = this._platform.platforms();
   }
 
 }
