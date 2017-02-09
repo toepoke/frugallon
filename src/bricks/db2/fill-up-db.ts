8208b8666dc814328f05da9f78af39c7071264b5
franz <franz@junge.f9.co.uk>
2017-02-09 13:57:26 +0000

    Filtering added and working

--- src/bricks/db2/fill-up-db.ts
+++ src/bricks/db2/fill-up-db.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { TypedDb, DbTypes, DbCmdFailure, DbCmdSuccess } from '../../core/typed-db/';
 import { FillUp, eFillUpType } from '../models';
+import { MpgStatDb } from './mpg-stat-db';
 import * as ditto from '../../core/helpers/ditto';
 
 @Injectable()
 export class FillUpDb extends TypedDb<FillUp> {
+	static TABLE_NAME: string = 'fill_ups';
 
 	constructor(
 		dbName: string,
 		provider: number
 	) {
-		super(FillUpDb.getSchema(), dbName, 'fill_ups', provider);
+		super(FillUpDb.getSchema(), dbName, FillUpDb.TABLE_NAME, provider);
 	}
 
 	static getSchema(): any {
@@ -66,16 +68,66 @@ export class FillUpDb extends TypedDb<FillUp> {
 	public getFiltered(yearFilters: Array<number>, journeyFilters: Array<eFillUpType>, carFilters: Array<number>, mpgAverages: Array<number>, activeMeasurement: boolean): Promise<Array<FillUp>> {
 		let sql: string = '';
 		let args: Array<any> = [];
+		let showBad: boolean = false, showNeutral: boolean = false, showGood: boolean = false;
 
-		sql += 'SELECT * FROM ' + this._tableName + ' ';
-		sql += 'WHERE ';
-		sql += '(';
-		sql +=   'CAST(substr([when],1,4) AS INTEGER) IN (' + yearFilters.join(',') + ')';
-		sql += ') AND (';
-		sql +=   'fillType IN (' + journeyFilters.join(',') + ')';
-		sql += ') AND (';
-		sql +=   'carId IN (' + carFilters.join(',') + ')';
-		sql += ')';
+		if (ditto.any(mpgAverages)) {
+			mpgAverages.forEach((value: number) => {
+				if (value < 0) {
+					showBad = true;
+				} else if (value > 0) {
+					showGood = true;
+				} else {
+					showNeutral = true;
+				}
+			});
+		}
+
+		sql = 
+			`SELECT DISTINCT fu.* \n` + 
+			`FROM ${this._tableName} fu \n` + 
+			`INNER JOIN ${MpgStatDb.TABLE_NAME} ms ON fu.carId = ms.carId \n` + 
+			`WHERE \n` + 
+			`    ( CAST(substr([when],1,4) AS INTEGER) IN ( ${yearFilters.join(",")} ) ) \n` + 
+			`AND ( fu.FillType IN ( ${journeyFilters.join(",")} ) ) \n` + 
+			`AND ( fu.carId IN ( ${carFilters.join(",")} ) ) \n`
+		;
+
+		sql += 'AND \n';
+		sql += '( \n';
+
+		if (!showBad && !showNeutral && !showGood) {
+			// SO basically we won't be showing ANY results!
+			sql += '   (1 = 2)\n';
+
+		} else {
+			let isFirst: boolean = true;
+			let measureField: string = (activeMeasurement ? 'metricMpg' : 'imperialMpg');
+
+			if (showBad) {
+				sql += `   (fu.${measureField} < ms.avg) /* below average */\n`;
+				isFirst = false;
+			}
+			if (showNeutral) {
+				let bottomEnd: string = '(ms.avg - (ms.avg * 0.1))';
+				let topEnd: string    = '(ms.avg + (ms.avg * 0.1))'
+				if (!isFirst) sql += '   OR ';
+				sql += `(fu.${measureField} BETWEEN ${bottomEnd} AND ${topEnd}) /* in average range */\n`
+				isFirst = false;
+			}
+			if (showGood) {
+				if (!isFirst) sql += '   OR ';
+				sql += `(fu.${measureField} > ms.avg) /* above average */\n`;
+				isFirst = false;
+			}
+
+			if (showBad || showNeutral || showGood) {
+				// If any MPG filters are applied, we'll need this for any of them (no point repeating the same filter 3 times!)
+				sql += `   AND (ms.type = "${activeMeasurement}") /* metric/imperial */\n`;
+			}
+
+		} // if !showing results
+
+		sql += ') \n';
 
 		return this.getByFilter(sql, args);
 	}
